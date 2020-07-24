dff9bec78baed42ef4cf477b84b6f6fd74720412
Renato Nobrega <renatonobrega87@gmail.com>
2020-07-24 16:16:53 -0300

    Initial commit

---
+++ src/Conta.java
@@ -0,0 +1,70 @@
+public abstract class Conta {
+
+    protected double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
+    private static int total;
+
+    public Conta(int agencia, int numero){
+        Conta.total++;
+        System.out.println("O total de contas é " + Conta.total);
+        this.agencia = agencia;
+        this.numero = numero;
+        this.saldo = 100;
+        System.out.println("Estou criando uma conta " + this.numero);
+    }
+
+    public abstract void deposita(double valor);
+
+    public void saca(double valor) throws SaldoInsuficienteException{
+        if (this.saldo < saldo){
+            throw new SaldoInsuficienteException("Saldo: " + this.saldo + ", Valor: " + valor);
+        }
+        this.saldo =- valor;
+    }
+
+    public void transfere(double valor, Conta destino) throws SaldoInsuficienteException {
+        this.saca(valor);
+        destino.deposita(valor);
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        if(this.numero <= 0){
+            System.out.println("Não pode valor menor igual a 0");
+        }
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        if (this.agencia <= 0){
+            System.out.println("Não pode valor menor igual a 0");
+            return;
+        }
+        this.agencia = agencia;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    public static int getTotal() {
+        return Conta.total;
+    }
+}
---
+++ src/TesteContas.java
@@ -0,0 +1,23 @@
+public class TesteContas {
+
+    public static void main(String[] args) throws SaldoInsuficienteException {
+
+        int a = 3;
+        int b = a / 0;
+
+        ContaCorrente outra = null;
+        outra.deposita(200);
+
+        ContaCorrente contaCorrente = new ContaCorrente(111, 111);
+        contaCorrente.deposita(100.0);
+
+        ContaPoupanca contaPoupanca = new ContaPoupanca(221, 222);
+        contaPoupanca.deposita(200.0);
+
+        contaCorrente.transfere(10.0, contaPoupanca);
+
+        System.out.println("Conta Corrente: " + contaCorrente.getSaldo());
+        System.out.println("Conta Poupanca: " + contaPoupanca.getSaldo());
+
+    }
+}
